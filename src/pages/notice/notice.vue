71fb9507fe2e0139f1d9f5b99dc02dc752e06eb2
billy <Billy@mask.io>
2022-02-22 02:19:07 +0800

    feat: notice

--- src/pages/notice/notice.vue
+++ src/pages/notice/notice.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="container">
     <CommonPageHeader :title="pageTitle" />
-    <div class="content">notice</div>
+    <div class="content">
+      <div class="border">
+        <img src="../../assets/notice/border.svg" alt="" />
+        <div class="inner">
+          <div v-for="item in noticeData" :key="item.key" class="inner_item">
+            <NoticeItem :info="item" />
+          </div>
+        </div>
+      </div>
+    </div>
     <CommonPageFooter />
   </div>
 </template>
@@ -10,16 +19,54 @@
 import { reactive,toRefs,onBeforeMount} from 'vue'
 import CommonPageHeader from '../../components/common_page_header'
 import CommonPageFooter from '../../components/common_page_footer'
+import NoticeItem from './notice_item'
 export default {
     name: 'store',
     components:{
         CommonPageHeader,
-        CommonPageFooter
+        CommonPageFooter,
+        NoticeItem
     },
       setup() {
 
           const data = reactive({
-            pageTitle:'公告栏'
+            pageTitle:'公告栏',
+            noticeData:[{
+              key:0,
+              time: new Date(),
+              title:'富贾三国公测',
+              content:'这是内容这是内容'
+            },
+            {
+              key:1,
+              time: new Date(),
+              title:'富贾三国公测',
+              content:'这是内容这是内容'
+            },
+            {
+              key:1,
+              time: new Date(),
+              title:'富贾三国公测',
+              content:'这是内容这是内容'
+            },
+            {
+              key:1,
+              time: new Date(),
+              title:'富贾三国公测',
+              content:'这是内容这是内容'
+            },
+            {
+              key:1,
+              time: new Date(),
+              title:'富贾三国公测',
+              content:'这是内容这是内容'
+            },
+            {
+              key:1,
+              time: new Date(),
+              title:'富贾三国公测',
+              content:'这是内容这是内容'
+            }]
           })
 
           onBeforeMount(() => {
@@ -37,6 +84,8 @@ export default {
 .container {
   width: 100%;
   height: 100%;
+  background: url("../../assets/notice/notice_bg.png") no-repeat;
+  background-size: cover;
 }
 .content {
   position: relative;
@@ -44,5 +93,23 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  .border {
+    position: relative;
+    width: 70rem;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .inner {
+      position: absolute;
+      width: 85%;
+      height: 80%;
+      overflow-x:hidden;
+      overflow-y: auto;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -45%);
+    }
+  }
 }
 </style>
---
+++ src/pages/notice/notice_item.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="item_box">
+    <div class="time">
+      {{ info.time }}
+    </div>
+    <div class="title">
+      {{ info.title }}
+    </div>
+    <div class="content">
+      {{ info.content }}
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+import { reactive,toRefs,onBeforeMount,onMounted} from 'vue'
+export default {
+    name: 'notice_item',
+    props:['info'],
+      setup() {
+          console.log('1-开始创建组件-setup')
+          const data = reactive({
+
+          })
+          onBeforeMount(() => {
+              console.log('2.组件挂载页面之前执行----onBeforeMount')
+          })
+          onMounted(() => {
+              console.log('3.-组件挂载到页面之后执行-------onMounted')
+          })
+          const refData = toRefs(data);
+          return {
+              ...refData,
+          }
+
+      }
+  };
+</script>
+<style lang="less" scoped>
+.item_box {
+  width: 100%;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+  text-align: left;
+  padding-bottom: 0.5rem;
+  margin-bottom: 0.5rem;
+  .time {
+    color: rgba(242, 219, 185, 0.5);
+  }
+  .title {
+    font-size: 2rem;
+    margin: 1rem 0;
+  }
+  .content {
+  }
+}
+</style>
