32f2bd113b23b40ca292703acea8f03a30640c6e
billy <Billy@mask.io>
2022-02-22 20:16:53 +0800

    feat: invite entry

--- src/pages/invite/invite.vue
+++ src/pages/invite/invite.vue
@@ -1,7 +1,51 @@
 <template>
   <div class="container">
     <CommonPageHeader :title="pageTitle" />
-    <div class="content">invite</div>
+    <div class="content">
+      <img class="bg_badge" src="../../assets/pack/bg_badge.svg" alt="" />
+      <div class="invite_box">
+        <div class="up">
+          <img src="../../assets/invite/up_bg.svg" alt="" />
+          <div class="info_box">
+            <div class="item title">
+              <img src="../../assets/invite/icons/1.svg" alt="" />
+              我的收益
+            </div>
+            <div class="item">
+              <div class="num">3000 <span class="xs">MMC</span></div>
+              <div class="des">日收益</div>
+            </div>
+            <div class="item">
+              <div class="num">12,000 <span class="xs">MMC</span></div>
+              <div class="des">总收益</div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom_box">
+          <div class="item">
+            <img src="../../assets/invite/item1_bg.svg" alt="" />
+            <div class="inner" >
+              <img src="../../assets/invite/icons/2.svg" alt="">
+              邀请排行榜
+            </div>
+          </div>
+          <div class="item">
+            <img src="../../assets/invite/item2_bg.svg" alt="" />
+            <div class="inner" >
+              <img src="../../assets/invite/icons/3.svg" alt="">
+              邀请规则
+            </div>
+          </div>
+          <div class="item">
+            <img src="../../assets/invite/item3_bg.svg" alt="" />
+            <div class="inner" >
+              <img src="../../assets/invite/icons/4.svg" alt="">
+              生成我的邀请码
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <CommonPageFooter />
   </div>
 </template>
@@ -44,5 +88,102 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  .bg_badge {
+    position: absolute;
+    width: 100%;
+    z-index: 1;
+    bottom: 15%;
+    opacity: 0.8;
+  }
+  .invite_box {
+    position: absolute;
+    width: 70rem;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -55%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 100;
+    .up {
+      position: relative;
+      cursor: pointer;
+      width: 100%;
+      margin-bottom: 1rem;
+      img {
+        width: 100%;
+      }
+      .title {
+        font-size: 2.5rem;
+        img {
+          width: 5rem;
+        }
+      }
+      .info_box {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-40%, -50%);
+        height: 80%;
+        width: 70%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .item {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          .num {
+            font-family: SF Pro Text;
+            font-size: 3rem;
+            font-style: italic;
+            font-weight: 700;
+            background: linear-gradient(48.4deg, #ffca7b 4.15%, #a67327 64.55%);
+            color: transparent;
+            background-clip: text;
+            .xs {
+              font-size: 1.5rem;
+            }
+          }
+          .des {
+            margin-top: 1rem;
+            font-size: 1.2rem;
+            color: white;
+            opacity: 0.5;
+          }
+        }
+      }
+    }
+    .bottom_box {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .item {
+        width: calc( 100%/3 - 1rem);
+        cursor: pointer;
+        position: relative;
+        img{
+          width: 100%;
+        }
+        .inner {
+          position: absolute;
+          width: 100%;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%,-50%);          
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          font-size: 2.5rem;
+          img {
+            width: 5rem;
+            margin-bottom: 1rem;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
