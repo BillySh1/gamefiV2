1a5098ccc6834d4b96d54d4ab41af28194343faf
billy <Billy@mask.io>
2022-02-25 00:45:53 +0800

    fix: fix

--- src/main.js
+++ src/main.js
@@ -2,13 +2,25 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import lottie from "vue-lottie";
-import store from './store/index';
-import { createToast } from 'mosha-vue-toastify';
-import './style/toast.less'
+import store from "./store/index";
+import { createToast } from "mosha-vue-toastify";
+import "./style/toast.less";
+const tp = require("tp-js-sdk");
 
 const _app = createApp(App);
-_app.config.globalProperties.$toast = createToast
+_app.config.globalProperties.$toast = createToast;
 _app.component("Lottie", lottie);
 _app.use(router);
-_app.use(store)
+_app.use(store);
+
+
+if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
+  tp.rollHorizontal({
+    horizontal: true,
+  });
+  tp.fullScreen({
+    fullScreen: 1,
+  });
+}
+
 _app.mount("#app");
