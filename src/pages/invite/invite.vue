8ae2fbdb3475fa2df831fad6900026302f23e833
billy <Billy@mask.io>
2022-03-11 21:12:48 +0800

    fix: fix

--- src/pages/invite/invite.vue
+++ src/pages/invite/invite.vue
@@ -112,7 +112,11 @@ export default {
     });
     const generate = () => {
       data.encodeData =
-        window.location.host + "/#/mint?invite=" + btoa(data.account);
+        "http://" +
+        window.location.host +
+        "/#/mint?invite=" +
+        btoa(data.account);
+      console.log(data.encodeData,'gggg')
       proxy.$toast("邀请链接已生成", store.state.toast_success);
       data.showModal = true;
     };
