3b491eb7450f40c4cb79c3240f1ad4c3064d40b5
billy <Billy@mask.io>
2022-03-24 01:26:39 +0800

    fix: fix

--- src/pages/all_stars/town/bf_pack.vue
+++ src/pages/all_stars/town/bf_pack.vue
@@ -17,15 +17,35 @@
         <div class="title">行军背包</div>
         <div class="left">
           <div class="inner">
-            <div class="empty">暂无道具</div>
+            <div
+              class="inner_item"
+              v-for="(item, index) in pack"
+              :key="index"
+              @click="() => (curItem = index)"
+              :style="curItem == index ? 'background: #93250d' : ''"
+            >
+              <img class="bg" :src="getItemDetail(index).img[0]" alt="" />
+              <div class="in">
+                <img :src="getItemDetail(index).img[1]" alt="" />
+              </div>
+              <div class="num">
+                {{ getItemDetail(index).name }} x {{ item }}
+              </div>
+            </div>
           </div>
         </div>
         <div class="right">
           <div class="inner">
             <div class="detail_img">
-              <img src="../../../allstar_assets/store/0_3.png" alt="" />
+              <img :src="getItemDetail(curItem).img[0]" alt="" />
+              <div class="in">
+                <img :src="getItemDetail(curItem).img[1]" alt="" />
+              </div>
             </div>
-            <div class="intro">龙刻运行 数量 x {{ num }}</div>
+            <div class="intro">
+              {{ getItemDetail(curItem).name }} 数量 x
+              {{ pack[curItem] }}
+            </div>
             <div class="intro">速率加成 {{ totalSpeedAdd }}</div>
             <div class="input_box">
               <img
@@ -66,7 +86,9 @@
                 "
               />
             </div>
-            <CommonButton class="btn" :disabled="true" :circle="true">使用</CommonButton>
+            <CommonButton class="btn" :disabled="true" :circle="true"
+              >使用</CommonButton
+            >
           </div>
         </div>
       </div>
@@ -76,8 +98,10 @@
 </template>
 
 <script >
-import { reactive, toRefs, onBeforeMount, onMounted, computed } from "vue";
+import { reactive, toRefs, onBeforeMount, computed } from "vue";
+import initWeb3 from "../../../utils/initWeb3";
 import CommonButton from "../../../components/common_button.vue";
+import { useStore } from "vuex";
 export default {
   name: "bf_pack",
   props: ["value"],
@@ -85,20 +109,78 @@ export default {
     CommonButton,
   },
   setup() {
+    const store = useStore();
     const data = reactive({
       num: 1,
+      account: "",
+      web3: "",
+      pack: [0, 0, 0, 0, 0],
+      curItem: 0,
     });
     const totalSpeedAdd = computed(() => {
       return 14400;
     });
-    onBeforeMount(() => {
+    onBeforeMount(async () => {
+      await initWeb3.Init(
+        (addr) => {
+          data.account = addr;
+        },
+        (p) => {
+          data.web3 = p;
+        }
+      );
+      await getPack();
     });
-    onMounted(() => {
-    });
+    const getItemDetail = (idx) => {
+      return [
+        {
+          img: [
+            require("../../../allstar_assets/main/pack/bg_0.png"),
+            require("../../../allstar_assets/main/pack/in_0.png"),
+          ],
+          name: "青铜骏马",
+        },
+        {
+          img: [
+            require("../../../allstar_assets/main/pack/bg_1.png"),
+            require("../../../allstar_assets/main/pack/in_1.png"),
+          ],
+          name: "白金汗血",
+        },
+        {
+          img: [
+            require("../../../allstar_assets/main/pack/bg_2.png"),
+            require("../../../allstar_assets/main/pack/in_2.png"),
+          ],
+          name: "银狐栖尊",
+        },
+        {
+          img: [
+            require("../../../allstar_assets/main/pack/bg_3.png"),
+            require("../../../allstar_assets/main/pack/in_3.png"),
+          ],
+          name: "白神象彝",
+        },
+        {
+          img: [
+            require("../../../allstar_assets/main/pack/bg_4.png"),
+            require("../../../allstar_assets/main/pack/in_4.png"),
+          ],
+          name: "龙刻陨星",
+        },
+      ][idx];
+    };
+
+    const getPack = async () => {
+      const c = store.state.c_battle_shop;
+      const res = await c.methods.ownList(data.account).call();
+      data.pack = res;
+    };
     const refData = toRefs(data);
     return {
       ...refData,
       totalSpeedAdd,
+      getItemDetail,
     };
   },
 };
@@ -176,12 +258,55 @@ export default {
         position: relative;
         width: 100%;
         height: 100%;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
         .empty {
           position: absolute;
           top: 50%;
           left: 50%;
           transform: translate(-50%, -50%);
           font-size: 1.3rem;
+        }
+        .inner_item {
+          cursor: pointer;
+          &:hover {
+            background: #93250d;
+          }
+          position: relative;
+          width: 10rem;
+          height: 10rem;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: rgba(147, 37, 13, 0.2);
+          border-radius: 8px;
+          .bg {
+            width: 6rem;
+            height: 6rem;
+          }
+          .in {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 3rem;
+            img {
+              width: 100%;
+            }
+          }
+          .num {
+            text-align: center;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            font-size: 1.2rem;
+            color: #ffdf3c;
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            transform: translate(-33%, 10%);
+          }
         }
       }
     }
@@ -200,9 +325,20 @@ export default {
         align-items: center;
         justify-content: space-around;
         .detail_img {
+          position: relative;
           width: 8rem;
           img {
             width: 100%;
+          }
+          .in {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 70%;
+            img {
+              width: 100%;
+            }
           }
         }
         .intro {
