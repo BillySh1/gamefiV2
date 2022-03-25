6f93318038a4bca5dbf3f2faf4ddc8f8b190872b
billy <Billy@mask.io>
2022-03-25 16:24:51 +0800

    fix: lint

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-module.exports = {
-  publicPath: "./",
+const { defineConfig } = require("@vue/cli-service");
+
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'http://18.167.19.112:8080/ipfs/Qme1fVFSqxNvM5UdNqh85U9orpcJRkycbNKhBokJ1jBU4K/game/'
+    : './',
   productionSourceMap: false,
-};
+});
