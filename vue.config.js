90562ca0bfe34f0066e3d0d558fcdc6f33c19e5d
billy <Billy@mask.io>
2022-03-25 16:28:09 +0800

    fix: fix

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
-
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'http://18.167.19.112:8080/ipfs/Qme1fVFSqxNvM5UdNqh85U9orpcJRkycbNKhBokJ1jBU4K/game/'
-    : './',
+module.exports = {
+  publicPath: './',
   productionSourceMap: false,
-});
+}
