4fdcc8813b8b91df35089d0f538e37562e1f36ae
billy <Billy@mask.io>
2022-04-02 16:37:25 +0800

    fix: fix

---
+++ src/pages/stake/entry_detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>222</div>
+</template>
+
+<script>
+import { reactive,toRefs,onBeforeMount,onMounted} from 'vue'
+export default {
+    name: 'stk_detail',
+      setup() {
+          const data = reactive({
+
+          })
+          onBeforeMount(() => {
+          })
+          onMounted(() => {
+          })
+          const refData = toRefs(data);
+          return {
+              ...refData,
+          }
+
+      }
+  };
+</script>
+<style lang='less' scoped>
+</style>
