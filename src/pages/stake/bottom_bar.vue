877014860f8148fedb39c0c4d245e4a1a61ef10c
billy <Billy@mask.io>
2022-04-02 16:09:41 +0800

    fix: fix

---
+++ src/pages/stake/bottom_bar.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="bottom">
+    <div class="badge_icon">
+      <img src="../../assets/stake/badge.png" alt="" />
+    </div>
+    <div class="title">
+      <div class="text">{{ title }}</div>
+      <img src="../../assets/stake/divider.png" alt="" />
+    </div>
+    <div
+      class="back_btn"
+      @click="
+        () =>
+          $router.push({
+            name: 'home',
+          })
+      "
+    >
+      <img src="../../assets/stake/back.png" alt="" />
+      <div class="text">主页</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs, onBeforeMount, onMounted } from "vue";
+export default {
+  props: ["title"],
+  name: "bottom_bar",
+  setup() {
+    const data = reactive({});
+    onBeforeMount(() => {});
+    onMounted(() => {});
+    const refData = toRefs(data);
+    return {
+      ...refData,
+    };
+  },
+};
+</script>
+<style lang='less' scoped>
+.bottom {
+  position: absolute;
+  left: 50%;
+  bottom: 5%;
+  width: 100%;
+  transform: translateX(-50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .badge_icon {
+    transform: translateX(40%);
+    img {
+      height: 6rem;
+    }
+  }
+  .title {
+    place-self: flex-end;
+    text-align: left;
+    width: 80%;
+    font-size: 1.5rem;
+    .text {
+      width: 100%;
+      padding-left: 4rem;
+      box-sizing: border-box;
+    }
+    img {
+      width: 100%;
+    }
+  }
+  .back_btn {
+    &:hover {
+      opacity: 0.8;
+    }
+    cursor: pointer;
+    position: relative;
+    width: 6rem;
+    img {
+      width: 100%;
+    }
+    .text {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 1.5rem;
+      color: white;
+    }
+  }
+}
+</style>
--- src/pages/stake/entry.vue
+++ src/pages/stake/entry.vue
@@ -22,27 +22,18 @@
     <div class="badge">
       <img src="../../assets/stake/bg.png" alt="" />
     </div>
-    <div class="bottom">
-      <div class="badge_icon">
-        <img src="../../assets/stake/badge.png" alt="" />
-      </div>
-      <div class="title">
-        <div class="text">攻城略地</div>
-        <img src="../../assets/stake/divider.png" alt="" />
-      </div>
-      <div class="back_btn" @click="()=>$router.push({
-          name:'home'})" >
-        <img src="../../assets/stake/back.png" alt="" />
-        <div class="text">主页</div>
-      </div>
-    </div>
+    <BottomBar title="攻城略地" />
   </div>
 </template>
 
 <script>
 import { reactive, toRefs, onBeforeMount, onMounted } from "vue";
+import BottomBar from "./bottom_bar.vue";
 export default {
   name: "stake_main",
+  components: {
+    BottomBar,
+  },
   setup() {
     const data = reactive({
       pools: [
@@ -165,58 +156,9 @@ export default {
     }
     .name {
       font-size: 1.5rem;
-      margin: 1.5rem 0;
+      margin: 1rem 0;
     }
     .apy {
-      color: white;
-    }
-  }
-}
-.bottom {
-  position: absolute;
-  left: 50%;
-  bottom: 5%;
-  width: 100%;
-  transform: translateX(-50%);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .badge_icon {
-    transform: translateX(40%);
-    img {
-      height: 6rem;
-    }
-  }
-  .title {
-    place-self: flex-end;
-    text-align: left;
-    width: 80%;
-    font-size: 1.5rem;
-    .text {
-      width: 100%;
-      padding-left: 4rem;
-      box-sizing: border-box;
-    }
-    img {
-      width: 100%;
-    }
-  }
-  .back_btn {
-    &:hover {
-      opacity: 0.8;
-    }
-    cursor: pointer;
-    position: relative;
-    width: 6rem;
-    img {
-      width: 100%;
-    }
-    .text {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      font-size: 1.5rem;
       color: white;
     }
   }
