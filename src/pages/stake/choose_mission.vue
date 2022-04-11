b440a08419c6a14341cc8b86e12a5aee7ed9a056
billy <Billy@mask.io>
2022-04-11 17:52:18 +0800

    fix: fix

--- src/pages/stake/choose_mission.vue
+++ src/pages/stake/choose_mission.vue
@@ -10,8 +10,25 @@
     </div>
     <div class="top_right">
       <img src="../../assets/stake/coin.png" alt="" />
-      <div class="text" >总奖池</div>
+      <div class="text">总奖池</div>
       <div class="text">154892734</div>
+    </div>
+    <div class="main_body">
+      <div class="item" v-for="(item, index) in map" :key="index">
+        <div class="mask">
+          <img src="../../assets/stake/detail/select_bg.png" alt="" />
+        </div>
+        <div class="scroll">
+          <img src="../../assets/stake/detail/scroll_bg.png" alt="" />
+          <div class="intro_text">
+            {{item.intro}}
+          </div>
+        </div>
+        <img class="icon" :src="item.img" alt="" />
+        <div class="text">执行 {{ item.name }} 任务</div>
+        <div class="text">为期 {{ item.time }} 天</div>
+        <div class="text">收益 {{ item.apy }}%</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,7 +42,31 @@ export default {
     StkBtn,
   },
   setup() {
-    const data = reactive({});
+    const data = reactive({
+      map: [
+        {
+          name: "斥候",
+          time: 7,
+          apy: 15,
+          img: require("../../assets/stake/detail/m_0.png"),
+          intro:'占位文案占位文案占位文案占位文案占位文案'
+        },
+        {
+          name: "扫荡",
+          time: 15,
+          apy: 15,
+          img: require("../../assets/stake/detail/m_1.png"),
+          intro:'占位文案占位文案占位文案占位文案占位文案'
+        },
+        {
+          name: "驻扎",
+          time: 30,
+          apy: 15,
+          img: require("../../assets/stake/detail/m_2.png"),
+          intro:'占位文案占位文案占位文案占位文案占位文案'
+        },
+      ],
+    });
     onBeforeMount(() => {});
     onMounted(() => {});
     const refData = toRefs(data);
@@ -79,20 +120,72 @@ export default {
     width: 10rem;
   }
 }
-.top_right{
-    position: absolute;
-    top: 3%;
-    right: 3%;
+.top_right {
+  position: absolute;
+  top: 3%;
+  right: 3%;
+  display: flex;
+  align-items: center;
+  img {
+    width: 4rem;
+    margin-right: 1.5rem;
+  }
+  .text {
+    font-size: 1.3rem;
+    margin-right: 1rem;
+  }
+}
+.main_body {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -60%);
+  display: flex;
+  align-items: center;
+  .item {
+    position: relative;
+    cursor: pointer;
+    .mask {
+      display: none;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 33rem;
+      img{
+          width: 100%;
+      }
+    }
+    &:hover {
+      .mask {
+        display: block;
+      }
+    }
     display: flex;
+    flex-direction: column;
     align-items: center;
-    img{
-        width: 4rem;
-        margin-right: 1.5rem;
+    margin: 0 3rem;
+    .scroll {
+      position: relative;
+      img {
+        width: 20rem;
+      }
+      .intro_text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: black;
+      }
     }
-    .text{
-        font-size: 1.3rem;
-        margin-right: 1rem;
+    .icon {
+      width: 3rem;
+      margin: 1.5rem 0;
     }
-
+    .text {
+      font-size: 1.2rem;
+      margin: 1rem 0;
+    }
+  }
 }
 </style>
