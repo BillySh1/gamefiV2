d625f93c3da72e0af702ec21b4e08deba8274a7b
billy <Billy@mask.io>
2022-10-08 14:03:24 +0800

    fix: some

--- src/utils/useHeroInfo.js
+++ src/utils/useHeroInfo.js
@@ -15,7 +15,7 @@ const usePropertyName = (x) => {
   return ["耐力", "力量", "智力", "敏捷", "精神"][x];
 };
 const useRarityName = (x) => {
-  return ["白", "蓝", "紫", "橙", "金", "红"][x];
+  return [t('rarity_0'), t('rarity_1'), t('rarity_2'), t('rarity_3'), t('rarity_4'), t('rarity_5')][x];
 };
 
 export {
